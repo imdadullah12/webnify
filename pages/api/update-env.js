6cbf81a5deac18bc89a688374d00de3dc7a7ccd7
Imdadullah Babu <imdadullahbabu01@gmail.com>
2023-02-19 21:08:55 +0530

    all set

--- pages/api/update-env.js
+++ pages/api/update-env.js
@@ -1,20 +1,29 @@
-import fs from "fs";
+// pages/api/update-env.js
 
-import { writeFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
-    const { databaseName } = req.body;
+    const { database_name } = req.body;
 
-    console.log("databaseName:", databaseName);
+    console.log("database_name:", database_name);
 
-    // Update the DATABASE_NAME value in the .env file
+    // Read the current contents of the .env file
     const envFilePath = `${process.cwd()}/.env`;
-    const envFileContent = `DATABASE_NAME=${process.env.DATABASE_NAME}\n`;
+    const envFileContent = await readFile(envFilePath, { encoding: "utf-8" });
 
+    // Replace the value of the DATABASE_NAME variable
+    const updatedEnvFileContent = envFileContent.replace(
+      /DATABASE_NAME=.*/,
+      `DATABASE_NAME=${database_name}`
+    );
+
+    // Write the updated contents to the .env file
     try {
-      await writeFile(envFilePath, envFileContent, { encoding: "utf-8" });
-      console.log(`Updated .env file with DATABASE_NAME=${databaseName}`);
+      await writeFile(envFilePath, updatedEnvFileContent, {
+        encoding: "utf-8",
+      });
+      console.log(`Updated .env file with DATABASE_NAME=${database_name}`);
       res.status(200).json({ message: "Database name updated successfully" });
     } catch (error) {
       console.error(error);
